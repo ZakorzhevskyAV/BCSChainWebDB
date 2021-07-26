e80087e0a12e9b41f19d8b33e1afdc72bc0ff813
ZakorzhevskyAV <zakvlzakvl@gmail.com>
2021-07-26 16:54:16 +0300

    fin
    fin

--- BCSChainWebSite/urls.py
+++ BCSChainWebSite/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('blocks/<int:block_height>', views.block_page, name='block_page'),
+    path('start_background', views.start_background, name='start_background'),
 ]
